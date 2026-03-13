45fb12d8c09af0a62214836677ea5149a5c3cb91
Nicolas <neco.beatles@gmail.com>
2026-03-13 16:46:56 -0300

    main added

---
+++ src/main.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Nicolas
+ */
+
+public class main {
+    public static void main(String[] args) {
+        java.awt.EventQueue.invokeLater(() -> {
+            new cadastroVIEW().setVisible(true);
+        });
+    }
+}
